d0c0044449178162699cf15cfc66071ab22b4be9
Lucio Nonato <luciononato@hotmail.com>
2020-05-29 00:07:10 -0300

    4 commit
    exibição automatizada

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -39,15 +39,20 @@ const Dashboard: React.FC = () => {
 
       const transactionsFormatted = response.data.transactions.map(
         (transaction: Transaction) => ({
-          ...transactions,
+          ...transaction,
           formattedValue: formatValue(transaction.value),
+          formattedDate: new Date(transaction.created_at).toLocaleDateString(
+            'pt-br',
+          ),
         }),
       );
 
+      const balanceInfo = response.data.balance;
+
       const balanceFormatted = {
-        income: formatValue(response.data.balance.income),
-        outcome: formatValue(response.data.balance.outcome),
-        total: formatValue(response.data.balance.total),
+        income: formatValue(balanceInfo.income),
+        outcome: formatValue(balanceInfo.outcome),
+        total: formatValue(balanceInfo.total),
       };
 
       setTransactions(transactionsFormatted);
@@ -97,18 +102,17 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome' && ' - '}
+                    {transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>20/04/2020</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
